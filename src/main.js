aad56d7eaa6e532bfb545c1cf35f075ae0ec7b64
Vincent Leclerc <vleclerc.dev@gmail.com>
2018-04-29 13:11:33 +0200

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,47 @@
+var http = require('http');
+
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({extended: true});
+
+var { exec } = require('child_process');
+
+var cors = require('cors');
+var corsOptions = {
+   origin: '*'
+}
+
+var express = require('express');
+var api = express();
+
+var apiName = 'webhook_node_api';
+var apiPort = 81;
+var localRepositoryPath = '/home/pi/raspibar';
+var eventPullRoute = '/payload';
+
+api.set( 'port', apiPort );
+api.use(urlencodedParser);
+api.use(bodyParser.json());
+api.use(cors(corsOptions))
+
+//index
+api.get('/', function(req, res) {
+        console.log(req.body);
+        res.json({apiName:apiName, apiPort:apiPort, localRepository:localRepository, eventPullRoute:eventPullRoute});
+});
+
+//pull event
+api.post(eventPullRoute, urlencodedParser, function(req, res) {
+  console.log(req.body.pusher.name + ' just pushed to ' + req.body.repository.name);
+  console.log('pulling code in '+localRepositoryPath);
+  exec('git -C ' + localRepositoryPath + ' pull -f', execCallback);
+  res.sendStatus(200);
+});
+
+http.createServer(api).listen(api.get('port'), function(){
+  console.log(apiName + ' listen on ' + apiPort);
+} );
+
+function execCallback(err, stdout, stderr) {
+  if(stdout) console.log(stdout);
+  if(stderr) console.log(stderr);
+}
