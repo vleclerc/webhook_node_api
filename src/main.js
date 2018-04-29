93560b84df50cb75230a5c4ffedc76a040512dc1
vleclerc <vleclerc.dev@gmail.com>
2018-04-29 14:52:28 +0200

    rename webhook route

--- src/main.js
+++ src/main.js
@@ -24,7 +24,7 @@ var deployConf = {
   host : host,
   port : apiPort,
   localRepositoryPath : '/home/pi/code/raspibar',
-  eventPullRoute : '/payload'
+  eventPullRoute : '/webhook'
 };
 
 api.set( 'port', apiPort );
